15255588aa91bc273aff39343778127a6811bd00
be876 <bernarderic876@gmail.com>
2023-05-22 12:47:00 +0200

    Initial Commit

---
+++ GitHubXcodeTest/GitHubXcodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubXcodeTestApp.swift
+//  GitHubXcodeTest
+//
+//  Created by User on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubXcodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
